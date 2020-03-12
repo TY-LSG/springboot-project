933b989579bbc2d589d7ea048bc6579462518786
lisg <728147616@qq.com>
2020-03-12 15:59:39 +0800

    初始化

--- src/main/java/com/lsg/SpringbootProjectApplication.java
+++ src/main/java/com/lsg/SpringbootProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootProjectApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootProjectApplication.class, args);
     }
